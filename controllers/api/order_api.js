feac6647df2a0875639696462a34f6042e71e1b0
MithunAj <mithunaj77@gmail.com>
2023-01-29 20:00:57 +0530

    final commit

--- controllers/api/order_api.js
+++ controllers/api/order_api.js
@@ -1,63 +1,53 @@
-const Order = require('./../../models/order');
+const Order = require("./../../models/order");
 
+module.exports.addOrder = async function (req, res) {
+  try {
+    if (!req.body.sub_total || isNaN(req.body.sub_total)) {
+      return res.status(400).json({
+        message: "Bad request",
+      });
+    } else {
+      await Order.create({
+        user_id: req.user._id,
+        phone: req.user.phone,
+        sub_total: req.body.sub_total,
+      });
 
-
-module.exports.addOrder = async function(req,res){
-    try {
-        
-        if(!req.body.sub_total || isNaN(req.body.sub_total)){
-            return res.status(400).json({
-                message : 'Bad request'
-            })
-        }else{
-            await Order.create({
-              user_id : req.user._id,
-              phone : req.user.phone,
-              sub_total : req.body.sub_total
-            })
-
-            return res.status(200).json({
-                message : 'Order created successfully'
-            })
-        }
-    } catch (error) {
-        console.log(error);
-        return res.json(500,{
-            message : 'Internal server error'
-        })
+      return res.status(200).json({
+        message: "Order created successfully",
+      });
     }
-    
-}
+  } catch (error) {
+    console.log(error);
+    return res.json(500, {
+      message: "Internal server error",
+    });
+  }
+};
 
-
-module.exports.getOrderDetails = async function(req,res){
-
-    try {
-
-        let orders = await Order.find({user_id : req.user._id},{_id : 0,user_id:0,__v:0});
-        // if the user does not have any orders we send status 404 and if not we send 200 with the orders array
-        if(orders.length === 0){
-
-            return res.status(404).json({
-                message : 'No orders to show'
-            })
-
-        }else{
-
-            return res.status(200).json({
-                message : 'Orders fetched',
-                data : {
-                    orders : orders
-                } 
-            })
-        }
-        
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message : 'Internal Server error'
-        })
+module.exports.getOrderDetails = async function (req, res) {
+  try {
+    let orders = await Order.find(
+      { user_id: req.user._id },
+      { user_id: 0, __v: 0 }
+    );
+    // if the user does not have any orders we send status 404 and if not we send 200 with the orders array
+    if (orders.length === 0) {
+      return res.status(404).json({
+        message: "No orders to show",
+      });
+    } else {
+      return res.status(200).json({
+        message: "Orders fetched",
+        data: {
+          orders: orders,
+        },
+      });
     }
-
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server error",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8010;
-const db = require('./config/mongoose');
-const bodyParser = require('body-parser');
-const passportJWT = require('./config/passport-jwt-strategy');
+const db = require("./config/mongoose");
+const bodyParser = require("body-parser");
+const passportJWT = require("./config/passport-jwt-strategy");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded());
-app.use('/',require('./routes/api/v1'));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(bodyParser.json());
+app.use("/", require("./routes/api/v1"));
 
 app.listen(port, function (err) {
   if (err) {
-    console.log('Could not start the server ', err);
+    console.log("Could not start the server ", err);
     return;
   }
 
-  console.log('Server is up and running on port : ', port);
+  console.log("Server is up and running on port : ", port);
 });
